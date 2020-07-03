cd9c9a183de615234f2f4487383abe06c872da55
Wuzup <dgr993@gmail.com>
2020-07-02 19:09:26 -0600

    added all the data for the five day forecast

--- api.js
+++ api.js
@@ -19,16 +19,42 @@ $.ajax({
     console.log(queryURL);
     console.log(response);
 
-    $(".firstdDay").text(JSON.stringify(response));
+    //info for first day of the 5 day forecast
     $(".firstDay .date").text(response.list[0].dt_txt.split(" ")[0]);
     $(".firstDay .weatherIcon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
-   
     var tempF = (response.list[0].main.temp- 273.15) * 1.80 + 32;
-
-    // add temp content to html
     $(".firstDay .temp").text("Temp: " + tempF.toFixed(0));
-    
-    // $(".firstDay .humidity").text("Wind Speed: " + response.wind.speed);
+    $(".firstDay .humidity").text("Humidity: " + response.list[0].main.humidity);
+
+    //info for second day of the 5 day forecast
+    $(".secondDay .date").text(response.list[0].dt_txt.split(" ")[0]);
+    $(".secondDay .weatherIcon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
+    var tempF = (response.list[0].main.temp- 273.15) * 1.80 + 32;
+    $(".secondDay .temp").text("Temp: " + tempF.toFixed(0));
+    $(".secondDay .humidity").text("Humidity: " + response.list[0].main.humidity);
+
+    //info for third day of the 5 day forecast
+    $(".thirdDay .date").text(response.list[0].dt_txt.split(" ")[0]);
+    $(".thirdDay .weatherIcon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
+    var tempF = (response.list[0].main.temp- 273.15) * 1.80 + 32;
+    $(".thirdDay .temp").text("Temp: " + tempF.toFixed(0));
+    $(".thirdDay .humidity").text("Humidity: " + response.list[0].main.humidity);
+
+    //info for fourth day of the 5 day forecast
+    $(".fourthDay .date").text(response.list[0].dt_txt.split(" ")[0]);
+    $(".fourthDay .weatherIcon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
+    var tempF = (response.list[0].main.temp- 273.15) * 1.80 + 32;
+    $(".fourthDay .temp").text("Temp: " + tempF.toFixed(0));
+    $(".fourthDay .humidity").text("Humidity: " + response.list[0].main.humidity);
+
+    //info for fifth day of the 5 day forecast
+    $(".fifthDay .date").text(response.list[0].dt_txt.split(" ")[0]);
+    $(".fifthDay .weatherIcon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
+    var tempF = (response.list[0].main.temp- 273.15) * 1.80 + 32;
+    $(".fifthDay .temp").text("Temp: " + tempF.toFixed(0));
+    $(".fifthDay .humidity").text("Humidity: " + response.list[0].main.humidity);
+
+
 });
 
 });
