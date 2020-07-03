973e6958e571de1eaa6d9ac855037fb9a3d51ab4
Wuzup <dgr993@gmail.com>
2020-07-02 18:35:30 -0600

    finished first day temp, day and icon

--- api.js
+++ api.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var APIKey = "cc3b19a5e219530ad82c36718f50e8c7";
 
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + "q=" + searchBox + ",us&appid=" + APIKey;
-console.log("this works")
+
 
 
 $.ajax({
@@ -19,9 +19,19 @@ $.ajax({
     console.log(queryURL);
     console.log(response);
 
-    $(".todayForecast").text(JSON.stringify(response));
-})
+    $(".firstdDay").text(JSON.stringify(response));
+    $(".firstDay .date").text(response.list[0].dt_txt.split(" ")[0]);
+    $(".firstDay .weatherIcon").attr("src", "http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + ".png");
+   
+    var tempF = (response.list[0].main.temp- 273.15) * 1.80 + 32;
 
-})
+    // add temp content to html
+    $(".firstDay .temp").text("Temp: " + tempF.toFixed(0));
+    
+    // $(".firstDay .humidity").text("Wind Speed: " + response.wind.speed);
+});
 
-})
+});
+
+});
+
